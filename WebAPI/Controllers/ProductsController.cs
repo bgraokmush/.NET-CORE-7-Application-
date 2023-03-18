f9b86a2042d1e49fbde6612a3df44033255b75c9
Bugra Okumus <saltuk.bugraokumus@gmail.com>
2023-03-18 22:03:47 +0300

    Added  API layer

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using Bussiness.Abstract;
+using Bussiness.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = _productService.GetAll();
+            if (result.Succes)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet]
+        public IActionResult Get(int id) {
+            var result = _productService.GetByID(id);
+
+            if (result.Succes)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Product product)
+        {
+            var resullt = _productService.Add(product);
+            if (resullt.Succes) {
+                return Ok(resullt.Message);
+            }
+            return BadRequest(resullt.Message);
+        }
+    }
+}
